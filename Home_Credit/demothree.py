bfa48ca18ea04daf2c8b86dee8e9c68fe33539af
ZB <1066816806@qq.com>
2018-08-12 06:55:52 +0800

    changge

--- Home_Credit/demothree.py
+++ Home_Credit/demothree.py
@@ -8,10 +8,20 @@ from sklearn.model_selection import train_test_split
 
 pd.set_option('display.max_columns', 103)
 basepath = r'D:\kaggle\Home Credit\data'
-del_columns = ["FLAG_DOCUMENT_2", "FLAG_DOCUMENT_3", "FLAG_DOCUMENT_4", "FLAG_DOCUMENT_5", "FLAG_DOCUMENT_6", "FLAG_DOCUMENT_7", "FLAG_DOCUMENT_8",
-               "FLAG_DOCUMENT_9", "FLAG_DOCUMENT_10", "FLAG_DOCUMENT_11", "FLAG_DOCUMENT_12", "FLAG_DOCUMENT_13", "FLAG_DOCUMENT_14", "FLAG_DOCUMENT_15",
-               "FLAG_DOCUMENT_16", "FLAG_DOCUMENT_17", "FLAG_DOCUMENT_18", "FLAG_DOCUMENT_19", "FLAG_DOCUMENT_20", "FLAG_DOCUMENT_21",
+del_columns = ["FLAG_DOCUMENT_2", "FLAG_DOCUMENT_3",
+               "FLAG_DOCUMENT_4", "FLAG_DOCUMENT_5",
+               "FLAG_DOCUMENT_6", "FLAG_DOCUMENT_7",
+               "FLAG_DOCUMENT_8", "FLAG_DOCUMENT_9",
+               "FLAG_DOCUMENT_10", "FLAG_DOCUMENT_11",
+               "FLAG_DOCUMENT_12", "FLAG_DOCUMENT_13",
+               "FLAG_DOCUMENT_14", "FLAG_DOCUMENT_15",
+               "FLAG_DOCUMENT_16", "FLAG_DOCUMENT_17",
+               "FLAG_DOCUMENT_18", "FLAG_DOCUMENT_19",
+               "FLAG_DOCUMENT_20", "FLAG_DOCUMENT_21",
                ]
+
+def logfunc(logit):
+    return 1 - 1 / (1 + np.exp(-x))
 
 # 训练数据集准备
 app = pd.read_csv(os.path.join(basepath,'application_train.csv'), header=0, sep=',')
@@ -38,7 +48,7 @@ data = data.fillna(np.random.rand())
 for col in data.select_dtypes(include=['object']).columns:
     data[col] = pd.Categorical(data[col]).codes
 x, y = data.iloc[:, 2:-1], data.iloc[:, 1]
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 
 # 测试数据集准备
@@ -50,7 +60,7 @@ for col in tdata.select_dtypes(include=['object']).columns:
     tdata[col] = pd.Categorical(tdata[col]).codes
 
 
-round = 100
+round = 50
 lr = 1
 learning_rate = [lr] * int(round * 0.4) + [lr * 0.5] * int(round * 0.3) + [lr * 0.25] * int(round * 0.2) + [lr * 0.1] * int(round * 0.1)
 train_data = xgb.DMatrix(x, label=y)
@@ -60,11 +70,12 @@ watch_list = [(train_data, 'train'), (test_data, 'test')]
 #           'objective': 'binary:logistic', 'num_class': 2,
 #           'booster': 'gblinear'}
 
-params = {'silent': 1,
-          'objective': 'reg:logistic',
+params = {'silent': 1, 'eta': 0.38,
+          'objective': 'binary:logitraw',
           'booster': 'gblinear'}
 xgbmodel = xgb.train(params=params, dtrain=train_data, num_boost_round=round,
-                     evals=watch_list)
+                     evals=watch_list,
+                     )
 # learning_rates=list(np.sort(np.logspace(-2, 0.01, round)))[::-1]
 
 
@@ -82,15 +93,18 @@ xgbmodel = xgb.train(params=params, dtrain=train_data, num_boost_round=round,
 x_test = xgb.DMatrix(tdata.iloc[:, 1:-1])
 idnum = tdata.iloc[:, 0]
 print(train_data.num_row(), train_data.num_col())
-for item in train_data.feature_names:
-    print(item)
+# for item in train_data.feature_names:
+#     print(item)
 print('train data'.center(80, '*'))
 
 print(x_test.num_row(), x_test.num_col())
-for feature in x_test.feature_names:
-    print(feature)
+# for feature in x_test.feature_names:
+#     print(feature)
 predict = xgbmodel.predict(x_test)
 print(type(predict), type(idnum))
 print(predict)
-# predictfile = pd.DataFrame({'SK_ID_CURR': idnum, 'TARGET': predict})
-# predictfile.to_csv(os.path.join(basepath, 'predict.csv'), index=False)
+
+
+store = list(map(logfunc, predict))
+predictfile = pd.DataFrame({'SK_ID_CURR': idnum, 'TARGET': store})
+predictfile.to_csv(os.path.join(basepath, 'predict.csv'), index=False)
